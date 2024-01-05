f9977b8ed03c108bf583733df00cff9fb8d1179e
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-01-04 20:20:21 -0500

    Add new test for callcode

--- tests/shanghai/callcode_test/test_callcode.py
+++ tests/shanghai/callcode_test/test_callcode.py
@@ -21,11 +21,16 @@ def test_callcode_with_gas(state_test: StateTestFiller, fork: Fork):
     pre = {
         "0x000000000000000000000000000000000000aaaa": Account(
             balance=0x03,
-            code="0x6000600060006000600130611a90f2",
+            code="0x6000600060006000600161AACC611A90F2",
+            nonce=1,
+        ),
+        "0x000000000000000000000000000000000000aaab": Account(
+            balance=0x03,
+            code="0x60008080808061AAAA613d50f1600055",
             nonce=1,
         ),
         "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b": Account(
-            balance=0x300000,
+            balance=0xfffffffff,
             nonce=0,
         ),
     }
@@ -34,19 +39,17 @@ def test_callcode_with_gas(state_test: StateTestFiller, fork: Fork):
         ty=1,
         chain_id=0x01,
         nonce=0,
-        to="0x000000000000000000000000000000000000aaaa",
+        to="0x000000000000000000000000000000000000aaab",
         value=1,
-        gas_limit=50000,
+        gas_limit=500000,
         gas_price=7,
         secret_key="0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8",
         protected=True,
     )
 
     post = {
-        "0x000000000000000000000000000000000000aaaa": Account(
-            code="0x6000600060006000600130611a90f2",
-            balance=4,
-            nonce=1,
+        "0x000000000000000000000000000000000000aaab": Account(
+            storage={0x00: 0x01}
         ),
     }
     state_test(env=env, pre=pre, post=post, txs=[tx])
@@ -63,11 +66,16 @@ def test_callcode_without_gas(state_test: StateTestFiller, fork: Fork):
     pre = {
         "0x000000000000000000000000000000000000aaaa": Account(
             balance=0x03,
-            code="0x6000600060006000600130611a90f2",
+            code="0x6000600060006000600161AACC611A90F2",
+            nonce=1,
+        ),
+        "0x000000000000000000000000000000000000aaab": Account(
+            balance=0x03,
+            code="0x60008080808061AAAA612d5ff1600055",
             nonce=1,
         ),
         "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b": Account(
-            balance=0x300000,
+            balance=0xfffffffff,
             nonce=0,
         ),
     }
@@ -76,19 +84,17 @@ def test_callcode_without_gas(state_test: StateTestFiller, fork: Fork):
         ty=0,
         chain_id=0x01,
         nonce=0,
-        to="0x000000000000000000000000000000000000aaaa",
+        to="0x000000000000000000000000000000000000aaab",
         value=1,
-        gas_limit=25000,
+        gas_limit=2500000,
         gas_price=7,
         secret_key="0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8",
         protected=True,
     )
 
     post = {
-        "0x000000000000000000000000000000000000aaaa": Account(
-            code="0x6000600060006000600130611a90f2",
-            balance=3,
-            nonce=1,
+        "0x000000000000000000000000000000000000aaab": Account(
+            storage={0x00: 0x00}
         ),
     }
     state_test(env=env, pre=pre, post=post, txs=[tx])
